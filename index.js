7926492aa67e513df349a81567d1cde8d1626c06
Connor <connor5741213@gmail.com>
2022-08-23 01:05:26 -0400

    last change

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ async function getFoursquare(business) {
 		Authorization: 'fsq3ATzZbmcGhdeFafr73wZcnJ+LlN6bK+4dh19a7ClS4u8='
 		}
 	}
-	let limit = 10
+	let limit = 5
 	let lat = myMap.coordinates[0]
 	let lon = myMap.coordinates[1]
 	let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)   //url uses backtics so values can be added in
